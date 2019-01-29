027acc2f44a40efba554393c51a034f0ed0b6629
boyszhen <1033437725@qq.com>
2019-01-29 22:35:09 +0800

    项目搭建

---
+++ smms/src/main.js
@@ -0,0 +1,14 @@
+import Vue from 'vue'
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css'
+import App from './App.vue'
+import router from './router'
+
+Vue.config.productionTip = false;
+//注册组件
+Vue.use(ElementUI);
+
+new Vue({
+  router,
+  render: h => h(App)
+}).$mount('#app');
